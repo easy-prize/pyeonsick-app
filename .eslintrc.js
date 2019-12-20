1ecc2f0a2ff2145bc6e0a7527ff474623a7b3f37
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-12-20 20:57:46 +0900

    chore(init): setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
+  extends: [
+    '@react-native-community',
+    'airbnb',
+  ],
+  rules: {
+    'prettier/prettier': 0,
+  },
   root: true,
-  extends: '@react-native-community',
-};
+}
